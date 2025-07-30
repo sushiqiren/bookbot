bd74709663b878179fe73ccc6114203b25fc932b
sushiqiren <hzfulideman@gmail.com>
2025-07-30 11:00:57 +1000

    update main.py and add stats.py

---
+++ bookdot/main.py
@@ -0,0 +1,39 @@
+from stats import get_num_words, get_chars_dict
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"'{item['char']}': {item['num']}")
+
+    print("--- End report ---")
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
