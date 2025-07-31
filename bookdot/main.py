011638bca71a7df99223364c4ce0aadcbe0414c5
sushiqiren <hzfulideman@gmail.com>
2025-07-31 10:54:35 +1000

    add Arguments function to main file

--- bookdot/main.py
+++ bookdot/main.py
@@ -1,34 +1,28 @@
-from stats import get_num_words, get_chars_dict
+import sys
+from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
-    print()
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
 
     for item in chars_sorted_list:
         if not item["char"].isalpha():
             continue
-        print(f"'{item['char']}': {item['num']}")
+        print(f"{item['char']}: {item['num']}")
 
-    print("--- End report ---")
-
-
-def sort_on(d):
-    return d["num"]
-
-
-def chars_dict_to_sorted_list(num_chars_dict):
-    sorted_list = []
-    for ch in num_chars_dict:
-        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
-    sorted_list.sort(reverse=True, key=sort_on)
-    return sorted_list
+    print("============= END ===============")
 
 
 def get_book_text(path):
